84c4123002cd3dd026fb37dd36392de131570233
anandgitacc <anand.akmail@gmail.com>
2023-01-31 09:04:00 +0530

    testone

---
+++ src/org/employee/Company.java
@@ -0,0 +1,17 @@
+package org.employee;
+
+public class Company {
+	
+	// private -> access the method within the class
+	// public -> access the method anywhere in project
+	
+	public void comName() {
+		System.out.println("Company name: Greens");
+	}
+	
+	public void comId() {
+		System.out.println("Company id: 1122");
+	}
+	
+
+}
---
+++ src/org/employee/EmployeeDetails.java
@@ -0,0 +1,28 @@
+package org.employee;
+
+
+public class EmployeeDetails {
+	
+	private void empName() {
+		System.out.println("Anand");
+	}
+	
+	private void empMobile() {
+		System.out.println("8939278836");
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		EmployeeDetails e = new EmployeeDetails();
+		e.empMobile();
+		e.empName();
+		
+		Company c = new Company();
+		c.comId();
+		c.comName();
+		
+		
+	}
+
+}
